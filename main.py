8a1677235828b9db0557c10f9133d3a38a930986
Abdullah Ekşi <104820840+abdullah-eksi@users.noreply.github.com>
2024-05-23 17:48:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -52,23 +52,23 @@ def main():
         print("")
         choice = input("Seçiminizi yapın: ")
     
-        if choice == "1":
-            username = input("Kullanıcı Adı: ")
-            password = input("Şifre: ")
-            user_id, authority = User.login(username, password, db)
+        match choice:
+            case "1":
+                username = input("Kullanıcı Adı: ")
+                password = input("Şifre: ")
+                user_id, authority = User.login(username, password, db)
 
-            if user_id:
-                # Giriş başarılı, Program Devam Ediyor
-                break
-        elif choice == "2":
-            username = input("Kullanıcı Adı: ")
-            email = input("E-posta: ")
-            password = input("Şifre: ")
-            new_user = User(username, email, password)  # Yetki parametresi verilmedi, varsayılan olarak '0' atanacak
-            new_user.register(db)
-            
-        else:
-            print("Geçersiz seçenek. Tekrar deneyin.")
+                if user_id:
+                    # Giriş başarılı, Program Devam Ediyor
+                    break
+            case "2":
+                username = input("Kullanıcı Adı: ")
+                email = input("E-posta: ")
+                password = input("Şifre: ")
+                new_user = User(username, email, password)  # Yetki parametresi verilmedi, varsayılan olarak '0' atanacak
+                new_user.register(db)
+            case _:
+                print("Geçersiz seçenek. Tekrar deneyin.")
     
     # Kullanıcı girişi başarılı işlemlere devam edebiliriz
     while True:
@@ -94,95 +94,93 @@ def main():
         print("0. Çıkış Yap \n")
 
         choice = input("Seçiminizi yapın: ")
-        #Seçeneğe Göre Tanımladığımız Metodları Çağırdık
-        if choice == "1":
-            print("\n--- Depo Ekle ---")
-            name = input("Depo Adı: ")
-            phone = input("Telefon: ")
-            email = input("E-posta: ")
-            address = input("Adres: ")
-            new_depot = Depot(name, phone, email, address)
-            new_depot.add_depot(db, user_id)
-            print("Depo başarıyla eklendi.")
-        elif choice == "2":
-            print("\n--- Depo Düzenle ---")
-            Depot.list_depots(db,user_id)
-            depot_id = int(input("Düzenlemek istediğiniz depo ID'sini girin: "))
-            depot = Depot("", "", "", "")  # Boş bir depo örneği oluşturduk, değerler kullanıcıdan alınacak
-            depot.edit_depot(db, user_id, depot_id)
-            print("Depo başarıyla Düzenlendi.")
-        elif choice == "3":
-            print("\n--- Depo Listele ---")
-            Depot.list_depots(db,user_id)
-        elif choice == "4":
-            print("\n--- Depo Kaldır ---")
-            Depot.list_depots(db,user_id)
-            depot_id = int(input("Kaldırmak istediğiniz depo ID'sini girin: "))
-            depot = Depot("", "", "", "")  # Boş bir depo örneği oluşturduk, değerler kullanıcıdan alınacak
-            depot.remove_depot(db, user_id, depot_id)
-        elif choice == "5":
-            print("\n--- Yeni Ürün Ekle ---")
-            name = input("Ürün Adı: ")
-            company = input("Firma Adı: ")
-            depot_no = int(input("Depo Numarası: "))
-            quantity = int(input("Adet: "))
-            weight = float(input("Ağırlık (kg): "))
-            brand = input("Marka: ")
-            new_product = Product(name, company, depot_no, quantity, weight, brand)
-            new_product.add_to_depot(db, user_id)
-            print("Ürün başarıyla eklendi.")
-        elif choice == "6":
-            print("\n--- Ürün Düzenle ---")
-            Product.list_products(db,user_id)
-            product_id = int(input("Düzenlemek istediğiniz ürün ID'sini girin: "))
-            product = Product("", "", 0, 0, 0.0, "")  # Boş bir ürün örneği oluşturduk, değerler kullanıcıdan alınacak
-            product.edit_product(db, user_id, product_id)
-        elif choice == "7":
-            print("\n--- Ürün Kaldır ---")
-            Product.list_products(db)
-            product_id = int(input("Kaldırmak istediğiniz ürün ID'sini girin: "))
-            product = Product("", "", 0, 0, 0.0, "")  # Boş bir ürün örneği oluşturduk, değerler kullanıcıdan alınacak
-            product.remove_product(db, user_id, product_id)
-        elif choice == "8":
-            print("\n--- Ürün Listele ---")
-            Product.list_products(db,user_id)
-        elif choice == "9":
-            print("\n--- Ürün Arama ---")
-            search_query = input("Aranacak ürün adı veya marka: ")
-            Product.search_product(db, search_query,user_id)    
-        elif choice == "0":
-            break
-        elif choice == "10" and authority == "1":  # Eğer kullanıcı yönetici ise
-            print("\n--- Yönetici Ekle ---")
-            username = input("Yönetici Kullanıcı Adı: ")
-            email = input("Yönetici E-posta: ")
-            password = input("Yönetici Şifre: ")
-            Depot.add_admin(db, username, email, password,user_id)
-            
-        elif choice == "11" and authority == "1":  # Eğer kullanıcı yönetici ise
-            print("\n--- Kullanıcı Listele ---")
-            User.list_users(db,user_id)
-           
-        elif choice == "12" and authority == "1":  # Eğer kullanıcı yönetici ise
-            print("\n--- Logları Excel'e Aktar ---")
-            print("1. Tüm Logları Excel'e Aktar")
-            print("2. İşlem Tipine Göre Logları Excel'e Aktar")
-            print("3. Kullanıcıya Göre Logları Excel'e Aktar")
-            export_choice = input("Seçiminizi yapın: ")
-            if export_choice == "1":
-                db.export_logs_to_excel()
-            elif export_choice == "2":
-                process_type = input("Listelenmesini istediğiniz işlem tipini girin: ")
-                db.export_logs_to_excel(process_type=process_type)
-            elif export_choice == "3":
-                user_id = input("Listelenmesini istediğiniz kullanıcının ID'sini girin (Tüm kullanıcılar için 'all' girin): ")
-                db.export_logs_to_excel(user_id=user_id)
-            else:
-                print("Geçersiz seçenek. Tekrar deneyin.")
-        else:
-            print("Geçersiz seçenek. Tekrar deneyin.")
-        
 
+        match choice:
+            case "1":
+                print("\n--- Depo Ekle ---")
+                name = input("Depo Adı: ")
+                phone = input("Telefon: ")
+                email = input("E-posta: ")
+                address = input("Adres: ")
+                new_depot = Depot(name, phone, email, address)
+                new_depot.add_depot(db, user_id)
+                print("Depo başarıyla eklendi.")
+            case "2":
+                print("\n--- Depo Düzenle ---")
+                Depot.list_depots(db, user_id)
+                depot_id = int(input("Düzenlemek istediğiniz depo ID'sini girin: "))
+                depot = Depot("", "", "", "")  # Boş bir depo örneği oluşturduk, değerler kullanıcıdan alınacak
+                depot.edit_depot(db, user_id, depot_id)
+                print("Depo başarıyla Düzenlendi.")
+            case "3":
+                print("\n--- Depo Listele ---")
+                Depot.list_depots(db, user_id)
+            case "4":
+                print("\n--- Depo Kaldır ---")
+                Depot.list_depots(db, user_id)
+                depot_id = int(input("Kaldırmak istediğiniz depo ID'sini girin: "))
+                depot = Depot("", "", "", "")  # Boş bir depo örneği oluşturduk, değerler kullanıcıdan alınacak
+                depot.remove_depot(db, user_id, depot_id)
+            case "5":
+                print("\n--- Yeni Ürün Ekle ---")
+                name = input("Ürün Adı: ")
+                company = input("Firma Adı: ")
+                depot_no = int(input("Depo Numarası: "))
+                quantity = int(input("Adet: "))
+                weight = float(input("Ağırlık (kg): "))
+                brand = input("Marka: ")
+                new_product = Product(name, company, depot_no, quantity, weight, brand)
+                new_product.add_to_depot(db, user_id)
+                print("Ürün başarıyla eklendi.")
+            case "6":
+                print("\n--- Ürün Düzenle ---")
+                Product.list_products(db, user_id)
+                product_id = int(input("Düzenlemek istediğiniz ürün ID'sini girin: "))
+                product = Product("", "", 0, 0, 0.0, "")  # Boş bir ürün örneği oluşturduk, değerler kullanıcıdan alınacak
+                product.edit_product(db, user_id, product_id)
+            case "7":
+                print("\n--- Ürün Kaldır ---")
+                Product.list_products(db)
+                product_id = int(input("Kaldırmak istediğiniz ürün ID'sini girin: "))
+                product = Product("", "", 0, 0, 0.0, "")  # Boş bir ürün örneği oluşturduk, değerler kullanıcıdan alınacak
+                product.remove_product(db, user_id, product_id)
+            case "8":
+                print("\n--- Ürün Listele ---")
+                Product.list_products(db, user_id)
+            case "9":
+                print("\n--- Ürün Arama ---")
+                search_query = input("Aranacak ürün adı veya marka: ")
+                Product.search_product(db, search_query, user_id)
+            case "0":
+                break
+            case "10" if authority == "1":
+                print("\n--- Yönetici Ekle ---")
+                username = input("Yönetici Kullanıcı Adı: ")
+                email = input("Yönetici E-posta: ")
+                password = input("Yönetici Şifre: ")
+                Depot.add_admin(db, username, email, password, user_id)
+            case "11" if authority == "1":
+                print("\n--- Kullanıcı Listele ---")
+                User.list_users(db, user_id)
+            case "12" if authority == "1":
+                print("\n--- Logları Excel'e Aktar ---")
+                print("1. Tüm Logları Excel'e Aktar")
+                print("2. İşlem Tipine Göre Logları Excel'e Aktar")
+                print("3. Kullanıcıya Göre Logları Excel'e Aktar")
+                export_choice = input("Seçiminizi yapın: ")
+                match export_choice:
+                    case "1":
+                        db.export_logs_to_excel()
+                    case "2":
+                        process_type = input("Listelenmesini istediğiniz işlem tipini girin: ")
+                        db.export_logs_to_excel(process_type=process_type)
+                    case "3":
+                        user_id = input("Listelenmesini istediğiniz kullanıcının ID'sini girin (Tüm kullanıcılar için 'all' girin): ")
+                        db.export_logs_to_excel(user_id=user_id)
+                    case _:
+                        print("Geçersiz seçenek. Tekrar deneyin.")
+            case _:
+                print("Geçersiz seçenek. Tekrar deneyin.")
 
     # Veritabanı Bağlantısını Kapat Ve Programı Bitir
     db.disconnect()
